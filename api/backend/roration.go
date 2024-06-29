f111bd66481ddc129863bca510ee1b364da9f187
jeffygu <jeffygu@163.com>
2024-06-29 23:31:34 +0800

    admin增删改查和session

--- api/backend/roration.go
+++ api/backend/roration.go
@@ -12,7 +12,33 @@ type RotationReq struct {
 }
 
 type RotationRes struct {
-	//todo
-	//g.Meta `mime:"text/html" example:"string"`
-	RotationId int `json:"rotationId"`
+	RotationId int `json:"rotation_id"`
+}
+
+type RotationDeleteReq struct {
+	g.Meta `path:"/backend/rotation/delete" method:"delete" tags:"轮播图" summary:"删除轮播图接口"`
+	Id     uint `v:"min:1#请选择需要删除的轮播图" dc:"轮播图id"`
+}
+type RotationDeleteRes struct{}
+
+type RotationUpdateReq struct {
+	g.Meta `path:"/backend/rotation/update/{Id}" method:"post" tags:"轮播图" summary:"修改轮播图接口"`
+	Id     uint   `json:"id"      v:"min:1#请选择需要修改的轮播图" dc:"轮播图Id"`
+	PicUrl string `json:"pic_url" v:"required#轮播图图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"    v:"required#跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"sort"    dc:"跳转链接"`
+}
+type RotationUpdateRes struct {
+	Id uint `json:"id"`
+}
+type RotationGetListCommonReq struct {
+	g.Meta `path:"/backend/rotation/list" method:"get" tags:"轮播图" summary:"轮播图列表接口"`
+	Sort   int `json:"sort"   in:"query" dc:"排序类型"`
+	CommonPaginationReq
+}
+type RotationGetListCommonRes struct {
+	List  interface{} `json:"list" description:"列表"`
+	Page  int         `json:"page" description:"分页码"`
+	Size  int         `json:"size" description:"分页数量"`
+	Total int         `json:"total" description:"数据总数"`
 }
--- internal/controller/rotation.go
+++ internal/controller/rotation.go
@@ -7,6 +7,7 @@ import (
 	"goframe-shop-v2/internal/service"
 )
 
+//承上启下  mvc
 // Rotation 内容管理
 var Rotation = cRotation{}
 
@@ -24,4 +25,37 @@ func (a *cRotation) Create(ctx context.Context, req *backend.RotationReq) (res *
 		return nil, err
 	}
 	return &backend.RotationRes{RotationId: out.RotationId}, nil
+}
+
+func (a *cRotation) Delete(ctx context.Context, req *backend.RotationDeleteReq) (res *backend.RotationDeleteRes, err error) {
+	err = service.Rotation().Delete(ctx, req.Id)
+	return
+}
+
+func (a *cRotation) Update(ctx context.Context, req *backend.RotationUpdateReq) (res *backend.RotationUpdateRes, err error) {
+	err = service.Rotation().Update(ctx, model.RotationUpdateInput{
+		Id: req.Id,
+		RotationCreateUpdateBase: model.RotationCreateUpdateBase{
+			PicUrl: req.PicUrl,
+			Link:   req.Link,
+			Sort:   req.Sort,
+		},
+	})
+	return &backend.RotationUpdateRes{Id: req.Id}, nil
+}
+
+func (a *cRotation) List(ctx context.Context, req *backend.RotationGetListCommonReq) (res *backend.RotationGetListCommonRes, err error) {
+	getListRes, err := service.Rotation().GetList(ctx, model.RotationGetListInput{
+		Page: req.Page,
+		Size: req.Size,
+		Sort: req.Sort,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &backend.RotationGetListCommonRes{List: getListRes.List,
+		Page:  getListRes.Page,
+		Size:  getListRes.Size,
+		Total: getListRes.Total}, nil
 }
